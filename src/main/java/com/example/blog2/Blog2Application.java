7d782f2d30dd3b9bb0fb0537f79ec62e79f0cddd
tek93 <=>
2020-06-03 19:30:42 +0200

    change

--- src/main/java/com/example/blog2/Blog2Application.java
+++ src/main/java/com/example/blog2/Blog2Application.java
@@ -1,13 +1,29 @@
 package com.example.blog2;
 
+import com.example.blog2.model.Post;
+import com.example.blog2.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Blog2Application {
+	@Autowired
+	PostRepository postRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Blog2Application.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public  void  get(){
+		Post post1 = new Post("What is Lorem Ipsum?","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "tomek");
+		Post post2 = new Post("What is Lorem Ipsum?","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", "elvis_presley");
+			postRepository.save(post1);
+			postRepository.save(post2);
+
+
 	}
 
 }
